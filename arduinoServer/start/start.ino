edcc08d8bf3ce8968b3c5ccd486efd2b71d6c159
Diogo Alves <56394360+Diogodalves@users.noreply.github.com>
2020-05-21 17:04:56 +0100

    Update start.ino

--- arduinoServer/start/start.ino
+++ arduinoServer/start/start.ino
@@ -19,9 +19,15 @@ void loop() {
     delay(50);
   }
   lastButtonState = buttonState;
-  
+  //After you read your sensor values (A0, A1 and A2)
   if (buttonPushCounter % 2 == 0) {
-    Serial.println(1);
+    Serial.print(1);
+    Serial.print(',');
+    Serial.print(A0);
+    Serial.print(',');
+    Serial.print(A1);
+    Serial.print(',');
+    Serial.printn(A2);
   } else {
     Serial.println(0);
   }
